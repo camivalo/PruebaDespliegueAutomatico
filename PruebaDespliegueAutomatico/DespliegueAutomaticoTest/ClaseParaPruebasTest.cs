e30562506ff210c9c4e70575d727f83acdb7d1d9
camilo <camivalo01@gmail.com>
2019-08-23 16:03:24 -0500

    tst

--- PruebaDespliegueAutomatico/DespliegueAutomaticoTest/ClaseParaPruebasTest.cs
+++ PruebaDespliegueAutomatico/DespliegueAutomaticoTest/ClaseParaPruebasTest.cs
@@ -14,7 +14,7 @@ namespace DespliegueAutomaticoTest
             //Arrange
             var a = 2;
             var b = 3;
-            int resultadoEsperado = 7;
+            int resultadoEsperado = 5;
 
             ClaseParaPruebas calculator = new ClaseParaPruebas();
 
